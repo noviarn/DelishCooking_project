042a6b992982a48194f41ab9ffe4d74727a03fb1
noviarn <42713448+noviarn@users.noreply.github.com>
2021-11-25 20:50:34 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'DelishCooking_project',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
